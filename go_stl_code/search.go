7fb8b25a28c2d390f59a4a0c18addd7087eadd34
JM-299792458 <puru.1si15cs081@gmail.com>
2021-03-16 04:17:10 +0530

    helper_func

---
+++ go_stl_code/search.go
@@ -0,0 +1,24 @@
+package gostlcode
+
+var ()
+
+//SLBSearch -> return the lower bound index using binary search
+func SLBSearch(a []int, v, l, h int) int {
+
+	if len(a) == 0 {
+		return 0
+	} else if v < a[l] {
+		return l
+	} else if v >= a[h] {
+		return h + 1
+	} else {
+		m := (l + h) / 2
+		if a[m] <= v {
+			l = m + 1
+		} else {
+			h = m - 1
+		}
+		return SLBSearch(a, v, l, h)
+	}
+
+}
