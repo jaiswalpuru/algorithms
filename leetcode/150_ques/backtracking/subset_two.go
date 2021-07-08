aa6d35c28e09d8701c757cbebe8f615002b2ed1f
jaiswalpuru <puru.1si15cs081@gmail.com>
2021-07-08 22:33:03 +0530

    backtracking

---
+++ leetcode/150_ques/backtracking/subset_two.go
@@ -0,0 +1,34 @@
+/*
+Generate power set of a set with the set having duplicates but power set should not have duplicates
+*/
+
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func power_set(arr []int) [][]int {
+	res := [][]int{}
+	subset := []int{}
+	sort.Ints(arr)
+	backtrack(arr, subset, 0, &res)
+	return res
+}
+
+func backtrack(arr []int, subset []int, start int, res *[][]int) {
+	*res = append(*res, subset)
+	for i := start; i < len(arr); i++ {
+		if i > start && arr[i] == arr[i-1] {
+			continue
+		}
+		temp := append(subset, arr[i])
+		backtrack(arr, temp, i+1, res)
+		temp = temp[0 : len(temp)-1]
+	}
+}
+
+func main() {
+	fmt.Println(power_set([]int{1, 2, 2}))
+}
--- leetcode/150_ques/backtracking/subsets.go
+++ leetcode/150_ques/backtracking/subsets.go
@@ -5,7 +5,10 @@ Generate all possible subsets of a given set.
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func power_set(arr []int) [][]int {
 	res := [][]int{}
@@ -24,6 +27,20 @@ func powerset(arr []int, subset []int, start int, res *[][]int) {
 	}
 }
 
+func power_set_non_recursive(arr []int) {
+	n := int(math.Pow(2, float64(len(arr))))
+	m := len(arr)
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if i&(1<<j) == 0 {
+				fmt.Print(arr[j], " ")
+			}
+		}
+		fmt.Println()
+	}
+}
+
 func main() {
 	fmt.Println(power_set([]int{1, 2, 3}))
+	power_set_non_recursive([]int{1, 2, 3})
 }
