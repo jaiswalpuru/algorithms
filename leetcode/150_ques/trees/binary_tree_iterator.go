04e11880ff474df1b9f131e307a55fdf7c6e31b3
jaiswalpuru <puru.1si15cs081@gmail.com>
2021-08-06 00:34:46 +0530

    binary tree iterator

---
+++ leetcode/150_ques/trees/binary_tree_iterator.go
@@ -0,0 +1,77 @@
+/*
+Implement the BSTIterator class that represents an iterator over the in-order traversal of a binary search
+tree (BST):
+
+BSTIterator(TreeNode root) Initializes an object of the BSTIterator class. The root of the BST is given
+as part of the constructor. The pointer should be initialized to a non-existent number smaller than any
+element in the BST.
+boolean hasNext() Returns true if there exists a number in the traversal to the right of the pointer,
+otherwise returns false.
+int next() Moves the pointer to the right, then returns the number at the pointer.
+Notice that by initializing the pointer to a non-existent smallest number, the first call to next() will
+return the smallest element in the BST.
+
+You may assume that next() calls will always be valid. That is, there will be at least a next number
+in the in-order traversal when next() is called.
+
+Example 1:
+Input
+["BSTIterator", "next", "next", "hasNext", "next", "hasNext", "next", "hasNext", "next", "hasNext"]
+[[[7, 3, 15, null, null, 9, 20]], [], [], [], [], [], [], [], [], []]
+Output
+[null, 3, 7, true, 9, true, 15, true, 20, false]
+
+Explanation
+BSTIterator bSTIterator = new BSTIterator([7, 3, 15, null, null, 9, 20]);
+bSTIterator.next();    // return 3
+bSTIterator.next();    // return 7
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 9
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 15
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 20
+bSTIterator.hasNext(); // return False
+*/
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func New(val int) *TreeNode { return &TreeNode{Val: val, Left: nil, Right: nil} }
+
+type BSTIterator struct {
+	root  *TreeNode
+	stack []*TreeNode
+}
+
+func Constructor(root *TreeNode) BSTIterator { return BSTIterator{root: root} }
+
+func (this *BSTIterator) Next() int {
+	for ; this.root != nil; this.root = this.root.Left {
+		this.stack = append(this.stack, this.root)
+	}
+
+	curr := this.stack[len(this.stack)-1]
+	this.stack = this.stack[:len(this.stack)-1]
+	this.root = curr.Right
+	return curr.Val
+}
+
+func (this *BSTIterator) HasNext() bool { return this.root != nil || len(this.stack) != 0 }
+
+func main() {
+	root := New(7)
+	root.Left = New(3)
+	root.Right = New(15)
+	root.Right.Left = New(9)
+	root.Right.Right = New(20)
+	obj := Constructor(root)
+	fmt.Println(obj.Next())
+	fmt.Println(obj.HasNext())
+}
