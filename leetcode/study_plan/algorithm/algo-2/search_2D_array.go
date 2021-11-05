12361c12b13edd67ce1f870959ab0dec96c3739d
jaiswalpuru <purujaiswal1996@gmail.com>
2021-11-04 20:33:45 -0500

    minor change

--- leetcode/study_plan/algorithm/algo-2/search_2D_array.go
+++ leetcode/study_plan/algorithm/algo-2/search_2D_array.go
@@ -3,6 +3,8 @@ Write an efficient algorithm that searches for a value in an m x n matrix. This 
 
 Integers in each row are sorted from left to right.
 The first integer of each row is greater than the last integer of the previous row.
+
+The below solution runs O(nlog(m)), where n is the number of rows and m if size of column.
 */
 
 package main
