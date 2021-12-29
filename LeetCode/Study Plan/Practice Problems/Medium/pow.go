af5ae1652d931d57e8b3d816963272496b838983
Puru Jaiswal <purujaiswal1996@gmail.com>
2021-12-29 16:33:49 -0600

    pow

---
+++ LeetCode/Study Plan/Practice Problems/Medium/pow.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func _pow(x float64, n int) float64 {
+	if n < 0 {
+		return 1 / pow(x, n)
+	}
+	return pow(x, n)
+}
+
+func pow(x float64, n int) float64 {
+	if n == 0 {
+		return 1
+	}
+	if n%2 == 0 {
+		half_ans := pow(x, n/2)
+		return half_ans * half_ans
+	} else {
+		half_ans := pow(x, n/2)
+		return half_ans * half_ans * x
+	}
+}
+
+func main() {
+	fmt.Println(_pow(2.00000, 10))
+}
