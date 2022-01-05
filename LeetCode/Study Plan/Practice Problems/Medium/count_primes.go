096951ba58ee8d5df596182d7c27ac9c65053926
Puru Jaiswal <purujaiswal1996@gmail.com>
2022-01-05 00:21:36 -0600

    count prime

---
+++ LeetCode/Study Plan/Practice Problems/Medium/count_primes.go
@@ -0,0 +1,41 @@
+/*
+Given an integer n, return the number of prime numbers that are strictly less than n.
+*/
+
+package main
+
+import "fmt"
+
+func sieve_of_eratosthenes(n int) []bool {
+	is_prime := make([]bool, n)
+	for i := 2; i < n; i++ {
+		is_prime[i] = true
+	}
+
+	for i := 2; i*i < n; i++ {
+		if !is_prime[i] {
+			continue
+		}
+		for j := i * i; j < n; j += i {
+			is_prime[j] = false
+		}
+	}
+	return is_prime
+}
+
+func count_prime(n int) int {
+	cnt := 0
+	is_prime := sieve_of_eratosthenes(n)
+
+	for i := 2; i < n; i++ {
+		if is_prime[i] {
+			cnt++
+		}
+	}
+
+	return cnt
+}
+
+func main() {
+	fmt.Println(count_prime(10))
+}
