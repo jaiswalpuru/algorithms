4031acd92c5ed87319bbc3266189f6bc60b9ebe7
Puru Jaiswal <purujaiswal1996@gmail.com>
2022-01-09 18:04:41 -0600

    permutation method 2 swapping

--- LeetCode/Study Plan/Practice Problems/Medium/permutations.go
+++ LeetCode/Study Plan/Practice Problems/Medium/permutations.go
@@ -10,6 +10,9 @@ func permutations(arr []int) [][]int {
 	res := [][]int{}
 	mp := make(map[int]struct{})
 	_permutations(arr, &res, []int{}, &mp)
+	ans := [][]int{}
+	_permutations_method_2(&arr, &ans, 0)
+	fmt.Println(ans)
 	return res
 }
 
@@ -27,6 +30,23 @@ func _permutations(arr []int, res *[][]int, set []int, visited *map[int]struct{}
 			delete(*visited, i)
 			temp = temp[:len(temp)-1]
 		}
+	}
+}
+
+//approach two
+func _permutations_method_2(arr *[]int, res *[][]int, start int) {
+	if start == len(*arr) {
+		*res = append(*res, append([]int{}, *arr...))
+		return
+	}
+
+	for i := start; i < len(*arr); i++ {
+		//swap
+		(*arr)[i], (*arr)[start] = (*arr)[start], (*arr)[i]
+		//recurse
+		_permutations_method_2(arr, res, start+1)
+		//swap back
+		(*arr)[i], (*arr)[start] = (*arr)[start], (*arr)[i]
 	}
 }
 
