a36dad92f6595c57d27d10c6e6aa0afd6b477c4c
Puru Jaiswal <purujaiswal1996@gmail.com>
2022-02-14 00:15:10 -0600

    zig zag traversal

---
+++ LeetCode/Study Plan/Practice Problems/Medium/Binary Tree Zig Zag Order Traversal/Go/binary_tree_zig_zag.go
@@ -0,0 +1,57 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val int
+	Left,
+	Right *TreeNode
+}
+
+func New(val int) *TreeNode { return &TreeNode{Val: val, Right: nil, Left: nil} }
+
+func zig_zag(root *TreeNode) [][]int {
+	if root == nil {
+		return nil
+	}
+	even := true
+	q := []*TreeNode{root}
+	res := [][]int{}
+	for len(q) > 0 {
+		temp := []int{}
+		n := len(q)
+		for i := 0; i < n; i++ {
+			curr := q[0]
+			q = q[1:]
+			temp = append(temp, curr.Val)
+			if curr.Left != nil {
+				q = append(q, curr.Left)
+			}
+			if curr.Right != nil {
+				q = append(q, curr.Right)
+			}
+		}
+		if !even {
+			reverse(&temp)
+		}
+		even = !even
+		res = append(res, temp)
+	}
+	return res
+}
+
+func reverse(t *[]int) {
+	n := len(*t)
+	for i := 0; i < n/2; i++ {
+		(*t)[i], (*t)[n-i-1] = (*t)[n-1-i], (*t)[i]
+	}
+}
+
+func main() {
+	root := New(3)
+	root.Left = New(9)
+	root.Right = New(20)
+	root.Right.Left = New(15)
+	root.Right.Right = New(7)
+	fmt.Println(zig_zag(root))
+}
