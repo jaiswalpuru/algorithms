7f13171b02025de4317b2a80288e94d95cb17c50
Puru Jaiswal <purujaiswal1996@gmail.com>
2022-02-18 14:14:35 -0600

    3 sum closet

--- LeetCode/Study Plan/Practice Problems/Medium/3 Sum Closet/Go/3_sum_closet.go
+++ LeetCode/Study Plan/Practice Problems/Medium/3 Sum Closet/Go/3_sum_closet.go
@@ -16,19 +16,22 @@ func abs(a int) int {
 func three_sum_closet(arr []int, target int) int {
 	sort.Ints(arr)
 	n := len(arr)
-	min_val := math.MaxInt64
+	min_val := math.MaxInt32
 	for i := 0; i < n; i++ {
 		j, k := i+1, n-1
 		for j < k {
 			sum := arr[i] + arr[j] + arr[k]
-			if abs(sum-target) < abs(min_val-target) {
-				min_val = sum
+			if abs(sum-target) <= abs(min_val) {
+				min_val = target - sum
 			}
-			j++
-			k--
+			if sum < target {
+				j++
+			} else {
+				k--
+			}
 		}
 	}
-	return min_val
+	return target - min_val
 }
 
 func main() {
