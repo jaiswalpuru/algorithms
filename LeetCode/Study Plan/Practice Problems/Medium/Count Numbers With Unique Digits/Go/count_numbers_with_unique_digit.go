b38ee0fffd668404112ce5361118399c4f125ec0
Puru Jaiswal <purujaiswal1996@gmail.com>
2022-02-25 20:06:09 -0600

    count numbers with unique digits

--- LeetCode/Study Plan/Practice Problems/Medium/Count Numbers With Unique Digits/Go/count_numbers_with_unique_digit.go
+++ LeetCode/Study Plan/Practice Problems/Medium/Count Numbers With Unique Digits/Go/count_numbers_with_unique_digit.go
@@ -35,27 +35,3 @@ func backtrack(curr int, count *int, visited *[]bool, n float64) {
 func main() {
 	fmt.Println(count_number_with_unique_digit(2))
 }
-
-func count_number_with_unique_digit_again(n int) int {
-	visited := make([]bool, 10)
-	count := 0
-	bactrack_again(0, &count, &visited, float64(n))
-	return count
-}
-
-func bactrack_again(curr int, count *int, visit *[]bool, n float64) {
-	if curr >= int(math.Pow(10, n)) {
-		return
-	}
-
-	(*count)++
-	for i := 0; i < 10; i++ {
-		if (i == 0 && curr == 0) || (*visit)[i] {
-			continue
-		}
-		(*visit)[i] = true
-		curr = 10*curr + i
-		backtrack(curr, count, visit, n)
-		(*visit)[i] = false
-	}
-}
