155128afaf780a3f937cf63976cfdeaf1b696669
Puru Jaiswal <purujaiswal1996@gmail.com>
2022-04-16 12:19:06 -0500

    design add and search word data structure

---
+++ LeetCode/Study Plan/Practice Problems/Medium/Design Add and Search Words Data Structure/Go/design_add_and_search_word_data_structure.go
@@ -0,0 +1,54 @@
+package main
+
+type TrieNode struct {
+	children [26]*TrieNode
+	is_word  bool
+}
+
+type WordDictionary struct {
+	root *TrieNode
+}
+
+func Constructor() WordDictionary {
+	return WordDictionary{root: &TrieNode{}}
+}
+
+func (this *WordDictionary) AddWord(word string) {
+	root := this.root
+	for _, ch := range word {
+		index := ch - 'a'
+		if root.children[index] == nil {
+			root.children[index] = &TrieNode{}
+		}
+		root = root.children[index]
+	}
+	root.is_word = true
+}
+
+func (this *WordDictionary) Search(word string) bool {
+	return _search(word, this.root)
+}
+
+func _search(word string, root *TrieNode) bool {
+	for k, ch := range word {
+		if ch == '.' {
+			for i := 0; i < len(root.children); i++ {
+				if root.children[i] != nil {
+					if _search(word[k+1:], root.children[i]) {
+						return true
+					}
+				}
+			}
+			return false
+		} else {
+			index := ch - 'a'
+			if root.children[index] == nil {
+				return false
+			}
+			root = root.children[index]
+		}
+	}
+	return root.is_word
+}
+
+func main() {}
