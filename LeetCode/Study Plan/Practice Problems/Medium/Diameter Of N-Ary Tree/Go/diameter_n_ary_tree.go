a567ebaa00d8096964953a360ecb95b5d278b9d5
Puru Jaiswal <purujaiswal1996@gmail.com>
2022-04-19 18:13:28 -0500

    diameter of n ary tree

---
+++ LeetCode/Study Plan/Practice Problems/Medium/Diameter Of N-Ary Tree/Go/diameter_n_ary_tree.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+import "sort"
+
+type Node struct {
+	Val      int
+	Children []*Node
+}
+
+func New(val int) *Node { return &Node{Val: val, Children: make([]*Node, 0)} }
+
+func max(a, b int) int {
+	if a>b {
+		return a
+	}
+	return b
+}
+
+func diameter_of_n_ary_tree(root *Node) int {
+	res := 0
+	dfs(root, &res)
+	return res
+}
+
+func dfs(root *Node, res *int) int {
+	max_arr := []int{0,0}
+	n := len(root.Children)
+	for i := 0; i < n; i++ {
+		curr := dfs(root.Children[i], res)
+		max_arr = append(max_arr, curr)
+	}
+
+	sort.Ints(max_arr)
+	*res = max(*res, max_arr[len(max_arr)-1]+max_arr[len(max_arr)-2])
+	return 1+max_arr[len(max_arr)-1]
+}
+
+func main() {
+	root := New(1)
+	root.Children = append(root.Children, New(3))
+	root.Children = append(root.Children, New(2))
+	root.Children = append(root.Children, New(4))
+	root.Children[0].Children = append(root.Children[0].Children, New(5))
+	root.Children[0].Children = append(root.Children[0].Children, New(6))
+	fmt.Println(diameter_of_n_ary_tree(root))
+}
