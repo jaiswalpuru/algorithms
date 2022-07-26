1970e67581b335eafb5f41ce40b33287b64f1ae9
Puru Jaiswal <purujaiswal1996@gmail.com>
2022-07-25 23:40:22 -0500

    lowest common ancestor of binary tree

--- LeetCode/Study Plan/Practice Problems/Medium/Lowest Common Ancestor Of A Binary Tree/Go/lowest_common_ancestor_of_a_binary_tree.go
+++ LeetCode/Study Plan/Practice Problems/Medium/Lowest Common Ancestor Of A Binary Tree/Go/lowest_common_ancestor_of_a_binary_tree.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 type TreeNode struct {
-	Val int
-	Left,
+	Val   int
+	Left  *TreeNode
 	Right *TreeNode
 }
 
@@ -17,30 +17,27 @@ func lowest_common_ancestor(root, p, q *TreeNode) *TreeNode {
 	if root.Val == p.Val || root.Val == q.Val {
 		return root
 	}
-
 	left := lowest_common_ancestor(root.Left, p, q)
 	right := lowest_common_ancestor(root.Right, p, q)
 	if left != nil && right != nil {
 		return root
 	}
-	if left != nil {
-		return left
-	} else {
+	if left == nil {
 		return right
+	} else {
+		return left
 	}
 }
 
 func main() {
 	root := New(3)
 	root.Left = New(5)
-	root.Right = New(1)
 	root.Left.Left = New(6)
 	root.Left.Right = New(2)
 	root.Left.Right.Left = New(7)
 	root.Left.Right.Right = New(4)
+	root.Right = New(1)
 	root.Right.Left = New(0)
 	root.Right.Right = New(8)
-	p := root.Left
-	q := root.Right
-	fmt.Println(lowest_common_ancestor(root, p, q))
+	fmt.Println(lowest_common_ancestor(root, root.Left, root.Right))
 }
--- LeetCode/Study Plan/Practice Problems/Medium/Lowest Common Ancestor Of A Binary Tree/README.md
+++ LeetCode/Study Plan/Practice Problems/Medium/Lowest Common Ancestor Of A Binary Tree/README.md
@@ -3,7 +3,7 @@
 ```
 Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
 
-According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).” 
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
 
 Example 1:
 
@@ -24,8 +24,8 @@ Output: 1
 
 Constraints:
 
-The number of nodes in the tree is in the range [2, 105].
--109 <= Node.val <= 109
+The number of nodes in the tree is in the range [2, 10^5].
+-10^9 <= Node.val <= 10^9
 All Node.val are unique.
 p != q
 p and q will exist in the tree.
