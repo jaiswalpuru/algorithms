cd0d8c30bf24f4875414cc9ba1cd6c558967f94e
Puru Jaiswal <purujaiswal1996@gmail.com>
2022-08-02 01:55:44 -0500

    kth largest element in array

--- LeetCode/Study Plan/Practice Problems/Medium/Kth Largest Element In Array/Go/kth_element_in_sorted_array.go
+++ LeetCode/Study Plan/Practice Problems/Medium/Kth Largest Element In Array/Go/kth_element_in_sorted_array.go
@@ -42,6 +42,40 @@ func largest_kth_ele(arr []int, k int) int {
 	return arr[len(arr)-k]
 }
 
+//---------------------------------using quick select approach----------------
+//considering the 0th index element as the pivot
+func partition(arr []int) int {
+	l, r := 0, len(arr)-1
+	for l < r {
+		if arr[l] >= arr[l+1] {
+			arr[l+1], arr[l] = arr[l], arr[l+1]
+			l++
+		} else if arr[l] < arr[r] {
+			r--
+		} else {
+			arr[l], arr[r] = arr[r], arr[l]
+		}
+	}
+	return l
+}
+
+func quick_select(arr []int, k int) int {
+	p := partition(arr)
+	if k == p {
+		return arr[p]
+	} else if k < p {
+		return quick_select(arr[:p], k)
+	} else {
+		return quick_select(arr[p+1:], k-(p+1))
+	}
+}
+
+func kth_largest_element_in_array(arr []int, k int) int {
+	return quick_select(arr, len(arr)-k)
+}
+
+//---------------------------------using quick select approach----------------
+
 func main() {
-	fmt.Println(kth_largest_element([]int{3, 2, 1, 5, 6, 4}, 2))
+	fmt.Println(kth_largest_element_in_array([]int{3, 2, 1, 5, 6, 4}, 2))
 }
