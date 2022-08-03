0a30d62cec593eef5a6ae21c2db3975516b94c65
Puru Jaiswal <purujaiswal1996@gmail.com>
2022-08-03 02:59:09 -0500

    kth smallest element in a sorted matrix

--- LeetCode/Study Plan/Practice Problems/Medium/Kth Smallest Element In A Sorted Matrix/Go/kth_smallest_element_in_sorted_matrix.go
+++ LeetCode/Study Plan/Practice Problems/Medium/Kth Smallest Element In A Sorted Matrix/Go/kth_smallest_element_in_sorted_matrix.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"container/heap"
 	"fmt"
 	"sort"
 )
@@ -22,8 +23,50 @@ func kth_smallest_element_in_sorted_matrix(arr [][]int, k int) int {
 
 //-----------------------------brute force------------------------------
 
+//-----------------------------using min heap---------------------------
+type Trip struct{ val, r, c int }
+type MH []Trip
+
+func (m MH) Less(i, j int) bool    { return m[i].val < m[j].val }
+func (m MH) Swap(i, j int)         { m[i], m[j] = m[j], m[i] }
+func (m MH) Len() int              { return len(m) }
+func (m *MH) Push(val interface{}) { *m = append(*m, val.(Trip)) }
+func (m *MH) Pop() interface{} {
+	val := (*m)[len(*m)-1]
+	*m = (*m)[:len(*m)-1]
+	return val
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+func kth_smallest_element_in_sorted_matrix_min_heap(arr [][]int, k int) int {
+	n := len(arr)
+	mh := make(MH, 0)
+	heap.Init(&mh)
+	for i := 0; i < min(n, k); i++ {
+		heap.Push(&mh, Trip{arr[i][0], i, 0})
+	}
+	ele := mh[0]
+	for k > 0 {
+		ele = heap.Pop(&mh).(Trip)
+		r, c := ele.r, ele.c
+		if c < n-1 {
+			heap.Push(&mh, Trip{arr[r][c+1], r, c + 1})
+		}
+		k--
+	}
+	return ele.val
+}
+
+//-----------------------------using min heap---------------------------
+
 func main() {
-	fmt.Println(kth_smallest_element_in_sorted_matrix([][]int{
+	fmt.Println(kth_smallest_element_in_sorted_matrix_min_heap([][]int{
 		{1, 5, 9}, {10, 11, 13}, {12, 13, 15},
 	}, 8))
 }
