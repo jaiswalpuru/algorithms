92aed72a0d6734c96d9fa25170b44e8df9de9903
Puru Jaiswal <purujaiswal1996@gmail.com>
2022-09-03 20:29:14 -0500

    binary tree max path sum

--- LeetCode/Study Plan/Practice Problems/Hard/Binary Tree Maximum Path Sum/Go/bt_max_path_sum.go
+++ LeetCode/Study Plan/Practice Problems/Hard/Binary Tree Maximum Path Sum/Go/bt_max_path_sum.go
@@ -14,13 +14,21 @@ type TreeNode struct {
 func New(val int) *TreeNode { return &TreeNode{Val: val, Left: nil, Right: nil} }
 
 func get_max_path_sum(root *TreeNode) int {
+	ans := math.MinInt64
+	recur(root, &ans)
+	return ans
+}
+
+func recur(root *TreeNode, ans *int) int {
 	if root == nil {
 		return 0
 	}
-
-	max_sum := math.MinInt64
-	_get_max(root, &max_sum)
-	return max_sum
+	left := recur(root.Left, ans)
+	right := recur(root.Right, ans)
+	l_sum, r_sum := max(root.Val, root.Val+left), max(root.Val, root.Val+right)
+	total_sum := l_sum + r_sum - root.Val
+	*ans = max(*ans, total_sum)
+	return max(l_sum, r_sum)
 }
 
 func max(a, b int) int {
@@ -28,17 +36,6 @@ func max(a, b int) int {
 		return a
 	}
 	return b
-}
-
-func _get_max(root *TreeNode, max_sum *int) int {
-	if root == nil {
-		return 0
-	}
-	left_gain := max(_get_max(root.Left, max_sum), 0)
-	right_gain := max(_get_max(root.Right, max_sum), 0)
-	path_sum := root.Val + left_gain + right_gain
-	*max_sum = max(*max_sum, path_sum)
-	return root.Val + max(left_gain, right_gain)
 }
 
 func main() {
