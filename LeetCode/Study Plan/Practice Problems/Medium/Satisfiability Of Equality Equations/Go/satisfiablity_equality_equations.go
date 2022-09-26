c6072efff21dec44e88f5be3d75ac20cc64411ff
Puru Jaiswal <purujaiswal1996@gmail.com>
2022-09-26 01:40:29 -0500

    satisfiability of equality equations

--- LeetCode/Study Plan/Practice Problems/Medium/Satisfiability Of Equality Equations/Go/satisfiablity_equality_equations.go
+++ LeetCode/Study Plan/Practice Problems/Medium/Satisfiability Of Equality Equations/Go/satisfiablity_equality_equations.go
@@ -47,6 +47,52 @@ func satisfy(equation []string) bool {
 	return true
 }
 
+//-------------------------------recursive----------------------------
+func satisfy_recursive(equation []string) bool {
+	graph := make(map[int][]int)
+	n := len(equation)
+
+	for i := 0; i < n; i++ {
+		if equation[i][1] == '=' {
+			a, b := int(equation[i][0]-'a'), int(equation[i][3]-'a')
+			graph[a] = append(graph[a], b)
+			graph[b] = append(graph[b], a)
+
+		}
+	}
+
+	color := make([]int, 26)
+	for i := 0; i < 26; i++ {
+		color[i] = -1
+	}
+	for i := 0; i < 26; i++ {
+		if color[i] == -1 {
+			dfs(i, i, &color, graph)
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		if equation[i][1] == '!' {
+			a, b := int(equation[i][0]-'a'), int(equation[i][3]-'a')
+			if color[a] == color[b] {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func dfs(node, count int, color *[]int, graph map[int][]int) {
+	if (*color)[node] == -1 {
+		(*color)[node] = count
+		for i := 0; i < len(graph[node]); i++ {
+			dfs(graph[node][i], count, color, graph)
+		}
+	}
+}
+
+//-------------------------------recursive----------------------------
+
 func main() {
-	fmt.Println(satisfy([]string{"a==b", "b==a"}))
+	fmt.Println(satisfy_recursive([]string{"a==b", "b==a"}))
 }
