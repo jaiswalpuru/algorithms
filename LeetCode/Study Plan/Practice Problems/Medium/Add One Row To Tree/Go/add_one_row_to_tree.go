97323cefe5d7002836020600fc59b5a74846df90
Puru Jaiswal <purujaiswal1996@gmail.com>
2022-10-04 20:06:06 -0500

    add one row to tree

--- LeetCode/Study Plan/Practice Problems/Medium/Add One Row To Tree/Go/add_one_row_to_tree.go
+++ LeetCode/Study Plan/Practice Problems/Medium/Add One Row To Tree/Go/add_one_row_to_tree.go
@@ -8,54 +8,56 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func New(val int) *TreeNode { return &TreeNode{Val: val, Left: nil, Right: nil} }
+func New(val int, left, right *TreeNode) *TreeNode {
+	return &TreeNode{Val: val, Left: left, Right: right}
+}
 
-func add_one_row(root *TreeNode, val int, d int) *TreeNode {
-	if d == 1 {
-		node := New(val)
-		node.Left = root
+type Pair struct {
+	node  *TreeNode
+	depth int
+}
+
+func add_one_row(root *TreeNode, val int, depth int) *TreeNode {
+	if depth == 1 {
+		node := New(val, root, nil)
 		return node
 	}
-
-	depth := 1
-	q := []*TreeNode{root}
-
-	for depth < d-1 {
-		temp := []*TreeNode{}
-		for len(q) > 0 {
-			curr := q[0]
-			q = q[1:]
-			if curr.Left != nil {
-				temp = append(temp, curr.Left)
-			}
-			if curr.Right != nil {
-				temp = append(temp, curr.Right)
+	q := []Pair{Pair{root, 1}}
+	f := false
+	for len(q) > 0 {
+		n := len(q)
+		for i := 0; i < n; i++ {
+			v := q[i]
+			if v.depth+1 == depth {
+				f = true
+				n1 := New(val, v.node.Left, nil)
+				n2 := New(val, nil, v.node.Right)
+				v.node.Left = n1
+				v.node.Right = n2
+			} else {
+				if v.node.Left != nil {
+					q = append(q, Pair{v.node.Left, v.depth + 1})
+				}
+				if v.node.Right != nil {
+					q = append(q, Pair{v.node.Right, v.depth + 1})
+				}
 			}
 		}
-		q = temp
-		depth++
-	}
-
-	for len(q) > 0 {
-		curr := q[0]
-		q = q[1:]
-		temp := curr.Left
-		curr.Left = New(val)
-		curr.Left.Left = temp
-		temp = curr.Right
-		curr.Right = New(val)
-		curr.Right.Right = temp
+		if f {
+			break
+		}
+		q = q[n:]
 	}
 	return root
 }
 
 func main() {
-	root := New(4)
-	root.Left = New(2)
-	root.Right = New(5)
-	root.Left.Left = New(3)
-	root.Left.Right = New(1)
-	root.Right.Left = New(8)
-	root.Right.Right = New(9)
-	fmt.Println(add_one_row(root, 1, 3).Right.Right.Right)
+	root := New(4, nil, nil)
+	root.Left = New(2, nil, nil)
+	root.Right = New(5, nil, nil)
+	root.Left.Left = New(3, nil, nil)
+	root.Left.Right = New(1, nil, nil)
+	root.Right.Left = New(8, nil, nil)
+	root.Right.Right = New(9, nil, nil)
+	fmt.Println(add_one_row(root, 1, 3))
 }
