bf9b3089c454751fbf401de64629c5c105ce8669
Puru Jaiswal <purujaiswal1996@gmail.com>
2022-10-21 09:16:20 -0500

    remove covered intervals

--- LeetCode/Study Plan/Practice Problems/Medium/Remove Covered Intervals/Go/remove_covered_intervals.go
+++ LeetCode/Study Plan/Practice Problems/Medium/Remove Covered Intervals/Go/remove_covered_intervals.go
@@ -5,6 +5,28 @@ import (
 	"sort"
 )
 
+//--------------------brute force-------------------------
+func remove_covered_intervals_brute(arr [][]int) int {
+	n := len(arr)
+	cnt := 0
+	mp := make(map[int]bool)
+	for i := 0; i < n; i++ {
+		curr := arr[i]
+		for j := 0; j < n; j++ {
+			if i != j && !mp[j] {
+				if curr[0] <= arr[j][0] && curr[1] >= arr[j][1] {
+					mp[j] = true
+					cnt++
+				}
+			}
+		}
+	}
+	return n - cnt
+}
+
+//--------------------brute force-------------------------
+
+//--------------------sorting-------------------------
 func remove_covered_intervals(intervals [][]int) int {
 	sort.Slice(intervals, func(i, j int) bool {
 		if intervals[i][0] == intervals[j][0] {
@@ -24,6 +46,8 @@ func remove_covered_intervals(intervals [][]int) int {
 	}
 	return cnt
 }
+
+//--------------------sorting-------------------------
 
 func main() {
 	fmt.Println(remove_covered_intervals([][]int{
