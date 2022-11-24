31bd3f03ef62544b9dfdd6af542234c76da00481
Puru Jaiswal <purujaiswal1996@gmail.com>
2022-11-23 23:48:10 -0600

    word search

--- LeetCode/Study Plan/Practice Problems/Medium/Word Search/Go/word_search.go
+++ LeetCode/Study Plan/Practice Problems/Medium/Word Search/Go/word_search.go
@@ -2,20 +2,19 @@ package main
 
 import "fmt"
 
-func search(board [][]byte, word string) bool {
-	m, n := len(board), len(board[0])
+var dir = [][]int{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}
 
-	visited := make([][]bool, m)
-	for i := 0; i < m; i++ {
-		visited[i] = make([]bool, n)
+func search(grid [][]byte, word string) bool {
+	n, m := len(grid), len(grid[0])
+	visited := make([][]bool, n)
+	for i := 0; i < n; i++ {
+		visited[i] = make([]bool, m)
 	}
 
-	word_len := len(word)
-
-	for i := 0; i < m; i++ {
-		for j := 0; j < n; j++ {
-			if board[i][j] == word[0] {
-				if dfs(board, word, i, j, 0, word_len, &visited) {
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if word[0] == grid[i][j] {
+				if dfs(i, j, grid, &visited, word, 0) {
 					return true
 				}
 			}
@@ -24,26 +23,21 @@ func search(board [][]byte, word string) bool {
 	return false
 }
 
-func dfs(board [][]byte, word string, i, j, k, word_len int, visited *[][]bool) bool {
-
-	if k == word_len {
+func dfs(i, j int, grid [][]byte, visited *[][]bool, word string, k int) bool {
+	if k == len(word) {
 		return true
 	}
-
-	if i < 0 || j < 0 || i >= len(board) || j >= len(board[0]) {
+	if i >= len(grid) || j >= len(grid[0]) || i < 0 || j < 0 || (*visited)[i][j] || k > len(word) {
 		return false
 	}
-
-	if (*visited)[i][j] {
+	if word[k] != grid[i][j] {
 		return false
 	}
-	if word[k] != board[i][j] {
-		return false
-	}
-
 	(*visited)[i][j] = true
-	res := dfs(board, word, i+1, j, k+1, word_len, visited) || dfs(board, word, i, j+1, k+1, word_len, visited) ||
-		dfs(board, word, i, j-1, k+1, word_len, visited) || dfs(board, word, i-1, j, k+1, word_len, visited)
+	res := false
+	for p := 0; p < 4; p++ {
+		res = res || dfs(i+dir[p][0], j+dir[p][1], grid, visited, word, k+1)
+	}
 	(*visited)[i][j] = false
 	return res
 }
