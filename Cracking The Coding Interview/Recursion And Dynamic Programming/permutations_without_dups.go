cf3c8f21d35a8fdc6d9b8bc6901d90981394c30e
Puru Jaiswal <purujaiswal1996@gmail.com>
2022-12-15 14:19:09 -0600

    permutation without dups

---
+++ Cracking The Coding Interview/Recursion And Dynamic Programming/permutations_without_dups.go
@@ -0,0 +1,57 @@
+package main
+
+import "fmt"
+
+//---------------------method 1--------------------------
+func permutations_without_dups(s string) []string {
+	res := []string{}
+	sb := []byte(s)
+	mp := make(map[byte]bool)
+	recur(sb, &mp, []byte{}, &res)
+	return res
+}
+
+func recur(sb []byte, mp *map[byte]bool, set []byte, res *[]string) {
+	if len(set) == len(sb) {
+		*res = append(*res, string(set))
+		return
+	}
+	for i := 0; i < len(sb); i++ {
+		if !(*mp)[sb[i]] {
+			t := append(set, sb[i])
+			(*mp)[sb[i]] = true
+			recur(sb, mp, t, res)
+			(*mp)[sb[i]] = false
+			t = t[:len(t)-1]
+		}
+	}
+}
+
+//---------------------method 1--------------------------
+
+//---------------------method 2--------------------------
+func permutations_without_dups_m2(s string) []string {
+	sb := []byte(s)
+	res := []string{}
+	_recur(sb, &res, 0)
+	return res
+}
+
+func _recur(sb []byte, res *[]string, ind int) {
+	if ind == len(sb) {
+		*res = append(*res, string(sb))
+		return
+	}
+	for i := ind; i < len(sb); i++ {
+		sb[i], sb[ind] = sb[ind], sb[i]
+		_recur(sb, res, ind+1)
+		sb[i], sb[ind] = sb[ind], sb[i]
+
+	}
+}
+
+//---------------------method 2--------------------------
+
+func main() {
+	fmt.Println(permutations_without_dups_m2("abc"))
+}
