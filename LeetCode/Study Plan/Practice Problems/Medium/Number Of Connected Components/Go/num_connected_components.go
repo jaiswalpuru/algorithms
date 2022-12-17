bf5c9141f9bee17ed9713bbc08fbeada065f8003
Puru Jaiswal <purujaiswal1996@gmail.com>
2022-12-17 01:05:40 -0600

    union find number of connected components

--- LeetCode/Study Plan/Practice Problems/Medium/Number Of Connected Components/Go/num_connected_components.go
+++ LeetCode/Study Plan/Practice Problems/Medium/Number Of Connected Components/Go/num_connected_components.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+//----------------------dfs-----------------------------
 func make_graph(edges [][]int) map[int][]int {
 	n := len(edges)
 	graph := make(map[int][]int)
@@ -36,8 +37,55 @@ func num_connected_components(n int, edges [][]int) int {
 	return components
 }
 
+//----------------------dfs-----------------------------
+
+//----------------------using union find-----------------------------
+func num_connected_components_union_find(n int, edges [][]int) int {
+	size := make([]int, n)
+	parent := make([]int, n)
+	for i := 0; i < n; i++ {
+		size[i] = i
+		parent[i] = i
+	}
+	for i := 0; i < len(edges); i++ {
+		union(edges[i][0], edges[i][1], &parent, &size)
+	}
+	num := 0
+	for i := 0; i < n; i++ {
+		if parent[i] == i {
+			num++
+		}
+	}
+	return num
+}
+
+func union(u, v int, parent *[]int, size *[]int) {
+	u = find(u, parent)
+	v = find(v, parent)
+	if u == v {
+		return
+	}
+	if (*size)[u] > (*size)[v] {
+		(*parent)[v] = u
+		(*size)[u] += (*size)[v]
+	} else {
+		(*parent)[u] = v
+		(*size)[v] += (*size)[u]
+	}
+}
+
+func find(u int, parent *[]int) int {
+	if u == (*parent)[u] {
+		return u
+	}
+	(*parent)[u] = find((*parent)[u], parent)
+	return (*parent)[u]
+}
+
+//----------------------using union find-----------------------------
+
 func main() {
-	fmt.Println(num_connected_components(2, [][]int{
+	fmt.Println(num_connected_components_union_find(2, [][]int{
 		{1, 0},
 	}))
 }
