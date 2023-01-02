b8d612ec7dffecb397c47096adf356f706442327
Puru Jaiswal <purujaiswal1996@gmail.com>
2023-01-02 10:58:15 -0600

    best team with no conflicts

--- LeetCode/Study Plan/Practice Problems/Medium/Best Team With No Conflicts/Go/best_with_no_conflicts.go
+++ LeetCode/Study Plan/Practice Problems/Medium/Best Team With No Conflicts/Go/best_with_no_conflicts.go
@@ -10,6 +10,53 @@ type Pair struct {
 	age int
 }
 
+//----------------------memo----------------------------
+func best_team_memo(scores []int, ages []int) int {
+	sc := make([]Pair, len(scores))
+	for i := 0; i < len(scores); i++ {
+		sc[i].age = ages[i]
+		sc[i].score = scores[i]
+	}
+	sort.Slice(sc, func(i, j int) bool {
+		if sc[i].age == sc[j].age {
+			return sc[i].score < sc[j].score
+		}
+		return sc[i].age < sc[j].age
+	})
+	memo := make([][]int, len(scores)+1)
+	for i := 0; i < len(scores)+1; i++ {
+		memo[i] = make([]int, len(scores)+1)
+		for j := 0; j < len(scores)+1; j++ {
+			memo[i][j] = -1
+		}
+	}
+	return recur(sc, &memo, 0, -1)
+}
+
+func recur(sc []Pair, memo *[][]int, start, prev int) int {
+	if start == len(sc) {
+		return 0
+	}
+	if (*memo)[prev+1][start] != -1 {
+		return (*memo)[prev+1][start]
+	}
+	if prev == -1 || sc[prev].score <= sc[start].score {
+		(*memo)[prev+1][start] = max(sc[start].score+recur(sc, memo, start+1, start), recur(sc, memo, start+1, prev))
+	} else {
+		(*memo)[prev+1][start] = recur(sc, memo, start+1, prev)
+	}
+	return (*memo)[prev+1][start]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+//----------------------memo----------------------------
+
 func best_team(scores []int, ages []int) int {
 	n := len(ages)
 	score_age := make([]Pair, n)
@@ -39,14 +86,7 @@ func best_team(scores []int, ages []int) int {
 	return ans
 }
 
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
-}
-
 func main() {
-	fmt.Println(best_team([]int{1, 3, 7, 3, 2, 4, 10, 7, 5},
-		[]int{4, 5, 2, 1, 1, 2, 4, 1, 4}))
+	fmt.Println(best_team_memo([]int{209, 41, 622, 584, 596, 781, 13, 953, 828, 115, 966, 969, 692, 64, 678, 349, 474, 314, 638, 224, 834, 750, 420, 428, 592, 16, 101, 346, 753, 413},
+		[]int{99, 69, 81, 58, 40, 44, 29, 46, 53, 87, 82, 91, 30, 37, 42, 24, 59, 86, 17, 83, 70, 5, 55, 54, 23, 88, 88, 44, 29, 17}))
 }
