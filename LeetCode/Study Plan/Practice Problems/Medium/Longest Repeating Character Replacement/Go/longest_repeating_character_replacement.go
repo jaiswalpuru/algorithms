067fa8ca318203d686046594e4199cee990372b1
Puru Jaiswal <purujaiswal1996@gmail.com>
2023-01-03 21:22:29 -0600

    longest repeating character replacement

--- LeetCode/Study Plan/Practice Problems/Medium/Longest Repeating Character Replacement/Go/longest_repeating_character_replacement.go
+++ LeetCode/Study Plan/Practice Problems/Medium/Longest Repeating Character Replacement/Go/longest_repeating_character_replacement.go
@@ -2,20 +2,55 @@ package main
 
 import "fmt"
 
-func longest_repeating_character_replacement(s string, k int) int {
-	l, max_freq := 0, 0
-	mp := make(map[byte]int)
-	res := 0
-	for r := 0; r < len(s); r++ {
-		mp[s[r]]++
-		max_freq = max(max_freq, mp[s[r]])
-		if r-l+1-max_freq > k {
-			mp[s[l]]--
-			l++
+//------------------------------using binary search----------------------------
+func longest_repeating_character_replacement_binary_search(s string, k int) int {
+	l, r := 1, len(s)+1
+	for l+1 < r {
+		mid := l + (r-l)/2
+		if valid(s, mid, k) {
+			l = mid
+		} else {
+			r = mid
 		}
-		res = r - l + 1
 	}
-	return res
+	return l
+}
+
+func valid(s string, substring_len, k int) bool {
+	freq := make(map[byte]int)
+	start := 0
+	max_freq := 0
+	for end := 0; end < len(s); end++ {
+		freq[s[end]]++
+		if end-start+1 > substring_len {
+			freq[s[start]]--
+			start++
+		}
+		max_freq = max(max_freq, freq[s[end]])
+		if substring_len-max_freq <= k {
+			return true
+		}
+	}
+	return false
+}
+
+//------------------------------using binary search----------------------------
+
+func longest_repeating_character_replacement(s string, k int) int {
+	win_start := 0
+	max_freq := 0
+	max_length := 0
+	freq := make(map[byte]int)
+	for win_end := 0; win_end < len(s); win_end++ {
+		freq[s[win_end]]++
+		max_freq = max(max_freq, freq[s[win_end]])
+		for win_end-win_start-max_freq+1 > k {
+			freq[s[win_start]]--
+			win_start++
+		}
+		max_length = max(max_length, win_end-win_start+1)
+	}
+	return max_length
 }
 
 func max(a, b int) int {
