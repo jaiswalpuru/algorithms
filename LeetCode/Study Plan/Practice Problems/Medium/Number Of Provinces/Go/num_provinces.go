41b794ea5aaf8ba4b3987157c3311477ecdf73c1
Puru Jaiswal <purujaiswal1996@gmail.com>
2023-01-04 11:32:29 -0600

    number of provcinces

--- LeetCode/Study Plan/Practice Problems/Medium/Number Of Provinces/Go/num_provinces.go
+++ LeetCode/Study Plan/Practice Problems/Medium/Number Of Provinces/Go/num_provinces.go
@@ -2,6 +2,56 @@ package main
 
 import "fmt"
 
+//------------------using union find---------------------
+func num_provinces_union_find(is_connected [][]int) int {
+	n := len(is_connected)
+	parent := make([]int, n)
+	size := make([]int, n)
+	for i := 0; i < n; i++ {
+		parent[i] = i
+		size[i] = 1
+	}
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			if is_connected[i][j] == 1 {
+				union(i, j, &parent, &size)
+			}
+		}
+	}
+	provinces := 0
+	for i := 0; i < n; i++ {
+		if parent[i] == i {
+			provinces++
+		}
+	}
+	return provinces
+}
+
+func union(x, y int, parent *[]int, size *[]int) {
+	x = find(x, parent)
+	y = find(y, parent)
+	if x == y {
+		return
+	}
+	if (*size)[x] > (*size)[y] {
+		(*parent)[y] = x
+		(*size)[x] += (*size)[y]
+	} else {
+		(*parent)[x] = y
+		(*size)[y] += (*size)[x]
+	}
+}
+
+func find(x int, parent *[]int) int {
+	if (*parent)[x] != x {
+		(*parent)[x] = find((*parent)[x], parent)
+	}
+	return (*parent)[x]
+}
+
+//------------------using union find---------------------
+
+//------------------using dfs---------------------
 func num_provinces(is_connected [][]int) int {
 	n := len(is_connected)
 	visited := make([][]bool, n)
@@ -29,6 +79,8 @@ func dfs(i, j int, is_connected [][]int, visited *[][]bool) {
 		}
 	}
 }
+
+//------------------dfs------------------------------
 
 func main() {
 	fmt.Println(num_provinces([][]int{
