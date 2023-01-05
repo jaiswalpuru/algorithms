58582bfc47dc53ef120278df5c72e4e592638ac4
Puru Jaiswal <purujaiswal1996@gmail.com>
2023-01-04 20:16:02 -0600

    graph valid tree

--- LeetCode/Study Plan/Practice Problems/Medium/Graph Valid Tree/Go/graph_valid_tree.go
+++ LeetCode/Study Plan/Practice Problems/Medium/Graph Valid Tree/Go/graph_valid_tree.go
@@ -2,6 +2,53 @@ package main
 
 import "fmt"
 
+//---------------union find----------------------
+func graph_valid_tree_union_find(n int, edges [][]int) bool {
+	parent := make([]int, n)
+	size := make([]int, n)
+	for i := 0; i < n; i++ {
+		parent[i] = i
+		size[i] = 1
+	}
+	for i := 0; i < len(edges); i++ {
+		if !union(edges[i][0], edges[i][1], &parent, &size) {
+			return false
+		}
+	}
+	cnt := 0
+	for i := 0; i < n; i++ {
+		if parent[i] == i {
+			cnt++
+		}
+	}
+	return !(cnt > 1)
+}
+
+func union(x, y int, parent *[]int, size *[]int) bool {
+	x = find(x, parent)
+	y = find(y, parent)
+	if x == y {
+		return false
+	}
+	if (*size)[x] >= (*size)[y] {
+		(*size)[x] += (*size)[y]
+		(*parent)[y] = x
+	} else {
+		(*size)[y] += (*size)[x]
+		(*parent)[x] = y
+	}
+	return true
+}
+
+func find(x int, parent *[]int) int {
+	if x != (*parent)[x] {
+		(*parent)[x] = find((*parent)[x], parent)
+	}
+	return (*parent)[x]
+}
+
+//---------------union find----------------------
+
 func make_graph(edges [][]int) map[int][]int {
 	g := make(map[int][]int)
 	n := len(edges)
