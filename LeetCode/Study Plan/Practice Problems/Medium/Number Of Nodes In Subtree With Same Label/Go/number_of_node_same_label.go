9c27bbe16520dcf908259bf10dd85e675bda1e86
Puru Jaiswal <purujaiswal1996@gmail.com>
2023-01-11 19:36:22 -0600

    number of nodes in subtree with same label

--- LeetCode/Study Plan/Practice Problems/Medium/Number Of Nodes In Subtree With Same Label/Go/number_of_node_same_label.go
+++ LeetCode/Study Plan/Practice Problems/Medium/Number Of Nodes In Subtree With Same Label/Go/number_of_node_same_label.go
@@ -4,10 +4,9 @@ import (
 	"fmt"
 )
 
-func make_graph(n int, edges [][]int, labels string) map[int][]int {
+func make_graph(edges [][]int) map[int][]int {
 	graph := make(map[int][]int)
-
-	for i := 0; i < n-1; i++ {
+	for i := 0; i < len(edges); i++ {
 		graph[edges[i][0]] = append(graph[edges[i][0]], edges[i][1])
 		graph[edges[i][1]] = append(graph[edges[i][1]], edges[i][0])
 	}
@@ -15,26 +14,27 @@ func make_graph(n int, edges [][]int, labels string) map[int][]int {
 }
 
 func num_nodes_subtree_with_same_label(n int, edges [][]int, labels string) []int {
-	graph := make_graph(n, edges, labels)
-	cnt := make([][26]int, n)
-
-	visited := make(map[int]bool)
-	dfs(0, graph, visited, labels, cnt)
+	g := make_graph(edges)
+	l := []byte(labels)
 	res := []int{}
-	for i := range cnt {
-		res = append(res, cnt[i][labels[i]-'a'])
+	visited := make(map[int]bool)
+	cnt := make([][26]int, n)
+	dfs(g, l, 0, &visited, &cnt)
+	for i := 0; i < n; i++ {
+		res = append(res, cnt[i][l[i]-'a'])
 	}
 	return res
 }
 
-func dfs(curr int, graph map[int][]int, visited map[int]bool, labels string, cnt [][26]int) {
-	visited[curr] = true
-	cnt[curr][labels[curr]-'a'] = 1
-	for k := 0; k < len(graph[curr]); k++ {
-		if !visited[graph[curr][k]] {
-			dfs(graph[curr][k], graph, visited, labels, cnt)
-			for i, val := range cnt[graph[curr][k]] {
-				cnt[curr][i] += val
+func dfs(g map[int][]int, l []byte, u int, visited *map[int]bool, count *[][26]int) {
+	(*count)[u][l[u]-'a'] = 1
+	(*visited)[u] = true
+	for i := 0; i < len(g[u]); i++ {
+		v := g[u][i]
+		if !(*visited)[v] {
+			dfs(g, l, v, visited, count)
+			for j := 0; j < 26; j++ {
+				(*count)[u][j] += (*count)[v][j]
 			}
 		}
 	}
