8c29c55fa1501beccb6821c8d8cf9ae34d0ecb5c
Puru Jaiswal <purujaiswal1996@gmail.com>
2023-01-14 02:51:17 -0600

    minimum cost to connect all points

--- LeetCode/Study Plan/Practice Problems/Medium/Minimum Cost To Connect All Points/Go/minimum_cost_to_connect_all_points.go
+++ LeetCode/Study Plan/Practice Problems/Medium/Minimum Cost To Connect All Points/Go/minimum_cost_to_connect_all_points.go
@@ -1,7 +1,9 @@
 package main
 
-import "fmt"
-import "container/heap"
+import (
+	"container/heap"
+	"fmt"
+)
 
 func abs(a int) int {
 	if a > 0 {
@@ -10,7 +12,7 @@ func abs(a int) int {
 	return -1 * a
 }
 
-func mahattan_distance(a, b []int) int { return abs(a[0]-b[0]) + abs(a[1]-b[1]) }
+func manhattan_distance(a, b []int) int { return abs(a[0]-b[0]) + abs(a[1]-b[1]) }
 
 type Point struct{ src, dst, dis int }
 
@@ -26,6 +28,7 @@ func (mh *MinHeap) Pop() interface{} {
 	return val
 }
 
+//-----------this is using prims algorithm--------------
 func min_cost_to_connect_all_points(arr [][]int) int {
 	n := len(arr)
 	visited := make([]bool, n)
@@ -46,15 +49,69 @@ func min_cost_to_connect_all_points(arr [][]int) int {
 			if visited[j] {
 				continue
 			}
-			cost := mahattan_distance(arr[i], arr[j])
+			cost := manhattan_distance(arr[i], arr[j])
 			heap.Push(mh, Point{i, j, cost})
 		}
 	}
 	return min_cost
 }
 
+//-----------this is using prims algorithm--------------
+
+//-----------kruskals algorithm--------------
+func min_cost_to_connect_all_points_kruskals(points [][]int) int {
+	mh := &MinHeap{}
+	for i := 0; i < len(points); i++ {
+		for j := i + 1; j < len(points); j++ {
+			dis := manhattan_distance(points[i], points[j])
+			heap.Push(mh, Point{i, j, dis})
+		}
+	}
+	parent := make([]int, len(points))
+	size := make([]int, len(points))
+	for i := 0; i < len(points); i++ {
+		parent[i] = i
+		size[i] = 1
+	}
+	n := len(points)
+	cost := 0
+	for mh.Len() > 0 && n > 0 {
+		curr := heap.Pop(mh).(Point)
+		if find(curr.src, &parent) != find(curr.dst, &parent) {
+			union(curr.src, curr.dst, &parent, &size)
+			cost += curr.dis
+			n--
+		}
+	}
+	return cost
+}
+
+func find(x int, parent *[]int) int {
+	if x != (*parent)[x] {
+		(*parent)[x] = find((*parent)[x], parent)
+	}
+	return (*parent)[x]
+}
+
+func union(x, y int, parent *[]int, size *[]int) {
+	x = find(x, parent)
+	y = find(y, parent)
+	if x == y {
+		return
+	}
+	if (*size)[x] > (*size)[y] {
+		(*size)[x] += (*size)[y]
+		(*parent)[y] = x
+	} else {
+		(*size)[y] += (*size)[x]
+		(*parent)[x] = y
+	}
+}
+
+//-----------kruskals algorithm--------------
+
 func main() {
-	fmt.Println(min_cost_to_connect_all_points([][]int{
+	fmt.Println(min_cost_to_connect_all_points_kruskals([][]int{
 		{0, 0}, {2, 2}, {3, 10}, {5, 2}, {7, 0},
 	}))
 }
