5a4897074567f325fc1a460fa64e7786c9ace81e
Puru Jaiswal <purujaiswal1996@gmail.com>
2023-01-15 19:15:14 -0600

    insert interval

--- LeetCode/Study Plan/Practice Problems/Medium/Insert Intervals/Go/insert_intervals.go
+++ LeetCode/Study Plan/Practice Problems/Medium/Insert Intervals/Go/insert_intervals.go
@@ -5,6 +5,46 @@ import (
 	"sort"
 )
 
+//-------------------without sorting-----------------------
+func insert_intervals_without_sort(intervals [][]int, new_interval []int) [][]int {
+	interval_mod := [][]int{}
+	res := [][]int{}
+	f := true
+	for i := 0; i < len(intervals); i++ {
+		if intervals[i][1] > new_interval[0] && f {
+			interval_mod = append(interval_mod, new_interval)
+			interval_mod = append(interval_mod, intervals[i])
+			f = false
+		} else {
+			interval_mod = append(interval_mod, intervals[i])
+		}
+	}
+	if f {
+		interval_mod = append(interval_mod, new_interval)
+	}
+	start := interval_mod[0]
+	for i := 1; i < len(interval_mod); i++ {
+		if start[1] >= interval_mod[i][0] {
+			start[0] = min(start[0], interval_mod[i][0])
+			start[1] = max(start[1], interval_mod[i][1])
+		} else {
+			res = append(res, start)
+			start = interval_mod[i]
+		}
+	}
+	res = append(res, start)
+	return res
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+//-------------------without sorting-----------------------
+
 func insert_intervals(interval [][]int, new_interval []int) [][]int {
 	interval = append(interval, new_interval)
 	sort.Slice(interval, func(i, j int) bool {
