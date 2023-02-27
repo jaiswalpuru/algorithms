fc89ff2c7c82e51c93d049df87f3eed2c43841b1
Puru Jaiswal <purujaiswal1996@gmail.com>
2023-02-27 12:13:02 -0600

    fixing topological sort DFs

--- Algorithms/Graphs/Go/topologicalSortDFS.go
+++ Algorithms/Graphs/Go/topologicalSortDFS.go
@@ -7,24 +7,42 @@ import "fmt"
 	Any node can be the start node.
 */
 
-func dfs(u int, visited *[]bool, order *[]int, g map[int][]int) {
-	(*visited)[u] = true
+const (
+	WHITE = 1
+	GRAY  = 2
+	BLACK = 3
+)
+
+func dfs(u int, visited *[]int, order *[]int, g map[int][]int, cycle *bool) {
+	(*visited)[u] = GRAY
 	for i := 0; i < len(g[u]); i++ {
-		if !(*visited)[g[u][i]] {
-			dfs(g[u][i], visited, order, g)
+		if (*visited)[g[u][i]] == WHITE {
+			dfs(g[u][i], visited, order, g, cycle)
+		} else if (*visited)[g[u][i]] == GRAY {
+			*cycle = true
 		}
 	}
 	*order = append(*order, u)
+	(*visited)[u] = BLACK
 }
 
 func topological_sort(g map[int][]int, n int) []int {
 	ordering := make([]int, n)
-	visited := make([]bool, n)
-	j := n - 1
+	visited := make([]int, n)
 	for i := 0; i < n; i++ {
-		if !visited[i] {
+		visited[i] = WHITE
+	}
+
+	j := n - 1
+	cycle := false
+	for i := 0; i < n; i++ {
+		if visited[i] == WHITE {
 			order := []int{}
-			dfs(i, &visited, &order, g)
+			dfs(i, &visited, &order, g, &cycle)
+			if cycle {
+				fmt.Println("Not a DAG")
+				return nil
+			}
 			for k := 0; k < len(order); k++ {
 				ordering[j] = order[k]
 				j--
