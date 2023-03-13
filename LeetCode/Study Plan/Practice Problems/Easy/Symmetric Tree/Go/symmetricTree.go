de64f3d9fab737fe806a2b427e58b246b4319d48
Puru Jaiswal <purujaiswal1996@gmail.com>
2023-03-12 23:33:40 -0500

    is symmetric

--- LeetCode/Study Plan/Practice Problems/Easy/Symmetric Tree/Go/symmetricTree.go
+++ LeetCode/Study Plan/Practice Problems/Easy/Symmetric Tree/Go/symmetricTree.go
@@ -13,25 +13,32 @@ type TreeNode struct {
 
 func New(val int) *TreeNode { return &TreeNode{Val: val, Left: nil, Right: nil} }
 
-func recur_is_symmetric(root *TreeNode) bool {
-	if root == nil {
-		return true
-	}
-	return _recur_is_symmetric(root.Left, root.Right)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSymmetric(root *TreeNode) bool {
+	return recur(root.Left, root.Right)
 }
 
-func _recur_is_symmetric(left, right *TreeNode) bool {
+func recur(left *TreeNode, right *TreeNode) bool {
 	if left == nil && right == nil {
 		return true
 	}
+
 	if left == nil || right == nil {
 		return false
 	}
+
 	if left.Val != right.Val {
 		return false
 	}
 
-	return _recur_is_symmetric(left.Left, right.Right) && _recur_is_symmetric(left.Right, right.Left)
+	return recur(left.Left, right.Right) && recur(left.Right, right.Left)
 }
 
 func main() {
@@ -42,5 +49,5 @@ func main() {
 	root.Left.Right = New(4)
 	root.Right.Left = New(4)
 	root.Right.Right = New(3)
-	fmt.Println(recur_is_symmetric(root))
+	fmt.Println(isSymmetric(root))
 }
