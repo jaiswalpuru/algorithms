dac5da9926ec0b9f84b6184637949a84d0550945
Puru Jaiswal <purujaiswal1996@gmail.com>
2023-04-02 12:54:57 -0500

    subsets two

---
+++ LeetCode/Study Plan/Practice Problems/Medium/Subsets II/Java/subsetsTwo.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(nums, 0, res, new ArrayList<>());
+        return res;
+    }
+
+    private void backtrack(int[] nums, int ind, List<List<Integer>> res, List<Integer> temp) {
+        res.add(new ArrayList<>(temp));
+        for (int i=ind; i<nums.length; i++) {
+            if (i > ind && nums[i] == nums[i-1]) continue;
+            temp.add(nums[i]);
+            backtrack(nums, i+1, res, temp);
+            temp.remove(temp.size()-1);
+        }
+    }
+}
