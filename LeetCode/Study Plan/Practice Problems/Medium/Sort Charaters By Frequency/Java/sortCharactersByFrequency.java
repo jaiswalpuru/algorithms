57eb3c4bb5f89855f89028bbab55783a1c89a102
Puru Jaiswal <purujaiswal1996@gmail.com>
2023-04-05 13:16:19 -0500

    sort characters by frequency

--- LeetCode/Study Plan/Practice Problems/Medium/Sort Charaters By Frequency/Java/sortCharactersByFrequency.java
+++ LeetCode/Study Plan/Practice Problems/Medium/Sort Charaters By Frequency/Java/sortCharactersByFrequency.java
@@ -1,3 +1,4 @@
+//------------------------method 1-----------------------------------
 class Solution {
     public String frequencySort(String s) {
         HashMap<Character, Integer> cntMap = new HashMap<>();
@@ -21,3 +22,34 @@ class Solution {
         return sb.toString();
     }
 }
+//------------------------method 1-----------------------------------
+
+//------------------------method 2-----------------------------------
+class Solution {
+    public String frequencySort(String s) {
+        PriorityQueue<Map.Entry<Character, Integer>> pq = new PriorityQueue<>(new Comparator<>(){
+            public int compare(Map.Entry<Character, Integer> a, Map.Entry<Character, Integer> b) {
+                return b.getValue()-a.getValue();
+            }
+        });
+
+        HashMap<Character, Integer> cntMap = new HashMap<>();
+        for (Character c : s.toCharArray())
+            cntMap.put(c, cntMap.getOrDefault(c, 0)+1);
+
+        for (Map.Entry<Character, Integer> m : cntMap.entrySet()) 
+            pq.add(m);
+        
+        StringBuilder sb = new StringBuilder();
+        while(!pq.isEmpty()) {
+            Map.Entry<Character, Integer> curr = pq.poll();
+            int cnt = curr.getValue();
+            while(cnt-- > 0) {
+                sb.append(curr.getKey());
+            }
+        }
+
+        return sb.toString();
+    }
+}
+//------------------------method 2-----------------------------------
