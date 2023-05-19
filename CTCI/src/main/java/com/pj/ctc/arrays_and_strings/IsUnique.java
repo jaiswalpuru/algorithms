81d66e3b107ab49f06530a1bcae0164b3e7cd5cd
Puru Jaiswal <purujaiswal1996@gmail.com>
2023-05-18 23:47:12 -0500

    structure for CTCI

---
+++ CTCI/src/main/java/com/pj/ctc/arrays_and_strings/IsUnique.java
@@ -0,0 +1,16 @@
+package com.pj.ctc.arrays_and_strings;
+
+/**
+ * Given a string, tell whether all the characters present in it are unique or not, what if you cannot use any
+ * additional data structure.
+ */
+
+public class IsUnique {
+	public static void main(String[] args) {
+
+	}
+
+	public static boolean isUnique(String str) {
+		return false;
+	}
+}
