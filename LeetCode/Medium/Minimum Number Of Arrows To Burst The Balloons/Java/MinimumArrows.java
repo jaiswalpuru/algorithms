8b9c61633758e50b8a79ef4aaf91749230cb61f5
Puru Jaiswal <purujaiswal1996@gmail.com>
2023-06-07 17:39:00 -0500

    minimum number of arrows to burst balloons

--- LeetCode/Medium/Minimum Number Of Arrows To Burst The Balloons/Java/MinimumArrows.java
+++ LeetCode/Medium/Minimum Number Of Arrows To Burst The Balloons/Java/MinimumArrows.java
@@ -1,36 +1,16 @@
-import java.util.Arrays;
-import java.util.Comparator;
-
-public class MinimumArrows{
-    public static void main(String[] args) {
-        int[][] balloons = {
-            {10,16},
-            {2,8},
-            {1,6},
-            {7,12}
-        };
-        System.out.println(MinimumArrowsShot(balloons, balloons.length));
-    }
-
-    public static int MinimumArrowsShot(int[][] balloons, int n){
-        Arrays.sort(balloons,new Comparator<int[]>() {
-            public int compare(int[] a, int[] b){
-                return Integer.compare(a[0], b[0]);
-            }
-        });
-
-        int maxEnd = balloons[0][1];
-        int minArrows = 1;
-        for (int i=1; i<n; i++){
-            if (balloons[i][0] <= maxEnd){
-                maxEnd = Math.min(maxEnd, balloons[i][1]);
-            }else{
-                minArrows++;
-                maxEnd = balloons[i][1];
+class Solution {
+    public int findMinArrowShots(int[][] points) {
+        int cnt = 1;
+        Arrays.sort(points, (a, b) -> Integer.compare(a[0], b[0]));
+        int track = points[0][1];
+        for (int i=1; i<points.length; i++) {
+            if (track < points[i][0]) {
+                cnt++;
+                track = points[i][1];
+            } else {
+                track = Math.min(track, points[i][1]);
             }
         }
-
-
-        return minArrows;
+        return cnt;
     }
 }
