ef3f7c511068e01ad62154b3501124926c4dad77
Puru Jaiswal <purujaiswal1996@gmail.com>
2023-07-14 19:12:11 -0700

    jump game

--- LeetCode/Medium/Jump Game/Java/jumpGame.java
+++ LeetCode/Medium/Jump Game/Java/jumpGame.java
@@ -1,11 +1,27 @@
+// recursive solution dp approach
 class Solution {
     public boolean canJump(int[] nums) {
-        int maxJump = nums[0];
-        int size = nums.length;
-        for (int i=1; i<size; i++) {
-            if (maxJump <= 0) return false;
-            maxJump = Math.max(maxJump-1, nums[i]);
+        return recur(nums, 1);
+    }
+
+    private boolean recur(int[] nums, int ind) {
+        if (ind >= nums.length) return true;
+        if (nums[ind-1] > 0) {
+            if (nums[ind-1] > nums[ind]) nums[ind] = nums[ind-1]-1;
+            return recur(nums, ind+1);
         }
-        return true;
+        return false;
+    }
+}
+
+//greedy approach
+class Solution {
+    public boolean canJump(int[] nums) {
+        int jump = nums[0];
+        for (int i=1; i<nums.length; i++) {
+            if (jump <= 0) return false; 
+            jump = Math.max(jump-1, nums[i]);
+        }
+        return jump >= 0;
     }
 }
