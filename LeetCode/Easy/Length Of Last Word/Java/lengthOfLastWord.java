519d7c3833fdc2623261748e30fec9e5a4dec353
Puru Jaiswal <purujaiswal1996@gmail.com>
2023-07-29 17:29:25 -0700

    length of the last word

---
+++ LeetCode/Easy/Length Of Last Word/Java/lengthOfLastWord.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String[] str = s.split(" ");
+        for (int i=str.length-1; i>=0; i--) if (!str[i].equals("")) return str[i].length();
+        return 0;
+    }
+}
