f1d5acbc61e03f4c5d10661044033b63e61dd766
Puru Jaiswal <purujaiswal1996@gmail.com>
2023-08-13 16:44:18 -0700

    reverse words in a string

--- LeetCode/Medium/Reverse Words In A String/Cpp/reverse_words_in_a_string.cc
+++ LeetCode/Medium/Reverse Words In A String/Cpp/reverse_words_in_a_string.cc
@@ -1,17 +1,10 @@
-#include <iostream>
-#include <vector>
-#include <sstream>
-
-std::string reverse_words_in_a_string(std::string s) {
-    std::stringstream ss(s);
-    std::string word;
-	std::string res = "";
-    while(ss>>word) {
-        res = word + " " + res;
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream ss(s);
+				string word;
+				string res = "";
+				while(ss >> word) res = word + " " + res;
+				return res.substr(0, res.size()-1);
     }
-    return res.substr(0, res.size()-1);
-}
-
-int main() {
-    std::cout<<reverse_words_in_a_string("the sky is blue")<<"\n";
-}
+};
