3f0bd558e261a15c574b06941decb8f94af83a04
Puru Jaiswal <purujaiswal1996@gmail.com>
2023-08-13 17:31:36 -0700

    increasing triplet subsequence

---
+++ LeetCode/Medium/Increasing Triplet Subsequence/C++/inc_triplet.cc
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream ss(s);
+		string word;
+		string res = "";
+		while(ss >> word) res = word + " " + res;
+		return res.substr(0, res.size()-1);
+    }
+};
