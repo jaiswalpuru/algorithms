3a5554b7d872f7469a5b93051d55011278a44477
Puru Jaiswal <purujaiswal1996@gmail.com>
2023-08-20 19:19:29 -0700

    rotting oranges

--- LeetCode/Medium/Rotting Oranges/Java/rottingOranges.java
+++ LeetCode/Medium/Rotting Oranges/Java/rottingOranges.java
@@ -1,45 +1,41 @@
-class P {
-    int x, y;
-    public P(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
-
 class Solution {
-    int[][] dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-
     public int orangesRotting(int[][] grid) {
-        Queue<P> q = new LinkedList<>();
         int n = grid.length, m = grid[0].length;
         boolean[][] visited = new boolean[n][m];
+
+        int minTime = -1;
+        ArrayDeque<Map.Entry<Integer, Integer>> q = new ArrayDeque<>();
         for (int i=0; i<n; i++) {
             for (int j=0; j<m; j++) {
-                if (grid[i][j] == 2) q.add(new P(i, j));
-            }
-        }
-
-        int min = -1;
-    
-        while(!q.isEmpty()) {
-            int size = q.size();
-            for (int i=0; i<size; i++) {
-                P curr = q.poll();
-                for (int k=0; k<4; k++) {
-                    int x = curr.x+dir[k][0];
-                    int y = curr.y+dir[k][1];
-                    if (x < 0 || y < 0 || x >= n || y >= m || visited[x][y] || grid[x][y] == 0 || grid[x][y] == 2) continue;
-                    grid[x][y] = 2;
-                    visited[x][y] = true;
-                    q.offer(new P(x, y));
+                if (grid[i][j] == 2) {
+                    q.offer(Map.entry(i, j));
                 }
             }
-            min++;
         }
 
-        for (int i=0; i<n; i++)
-            for (int j=0; j<m; j++) if (grid[i][j] == 1) return -1;
+        int[][] dir  = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        while (!q.isEmpty()) {
+            int size = q.size();
+            while(size-- > 0) {
+                Map.Entry<Integer, Integer> mp = q.poll();
+                int i = mp.getKey(), j = mp.getValue();
+                for (int k=0; k<4; k++) {
+                    int r = i + dir[k][0];
+                    int c = j + dir[k][1];
+                    if (r < 0 || c < 0 || r >= n || c >= m || visited[r][c] || grid[r][c] == 0 || grid[r][c] == 2) continue;
+                    grid[r][c] = 2;
+                    visited[r][c] = true;
+                    q.offer(Map.entry(r, c));
+                }
+            }
+            minTime++;
+        }
 
-        return min == -1 ? 0 : min;
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<m; j++) {
+                if (grid[i][j] == 1) return -1;
+            }
+        }
+        return minTime == -1 ? 0 : minTime;
     }
 }
