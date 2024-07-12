b2564fc97c0ce69e6fd84f61e234f7010863ce3d
Puru Jaiswal <p96jcodes@gmail.com>
2024-07-12 02:18:55 -0700

    three consecutive ones

---
+++ LeetCode/Easy/Three Consecutive Odds/c++/three_consecutive_ones.cc
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        if (arr.size() < 3) return false;
+
+        for (int i = 0; i < arr.size(); i++) {
+            if (i + 2 >= arr.size()) return false;
+            if (arr[i] % 2 != 0 && arr[i+1] % 2 != 0 && arr[i+2] % 2 != 0) return true;
+        }
+        return false;
+    }
+};
