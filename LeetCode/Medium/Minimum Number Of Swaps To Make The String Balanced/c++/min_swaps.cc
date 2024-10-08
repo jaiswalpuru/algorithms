3adf08324df903e08f254446381869f75391bf5e
Puru Jaiswal <p96jcodes@gmail.com>
2024-10-07 18:24:03 -0700

    minimum number of swaps to make the string balanced

---
+++ LeetCode/Medium/Minimum Number Of Swaps To Make The String Balanced/c++/min_swaps.cc
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minSwaps(string s) {
+        stack<char> st;
+        int unbalanced = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '[') {
+                st.push(s[i]);
+            } else {
+                if (!st.empty()) st.pop();
+                else unbalanced++;
+            }
+        }
+
+        return (unbalanced + 1) / 2;
+    }
+};
