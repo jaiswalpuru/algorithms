d4458c42fd938326d23037c87a80bef6752d2293
Puru Jaiswal <p96jcodes@gmail.com>
2024-10-07 18:34:16 -0700

    minimum add to make parantheses balanced

---
+++ LeetCode/Medium/Minimum Add To Make Parantheses Valid/c++/min_add.cc
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char> st;
+        int unbal = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(') {
+                st.push(s[i]);
+            } else {
+                if (!st.empty()) st.pop();
+                else unbal++;
+            }
+        }
+
+        return unbal + st.size();
+    }
+};
