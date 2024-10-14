cc0c36abb7442e55e709b7bb7062929bcfa063d8
Puru Jaiswal <p96jcodes@gmail.com>
2024-10-13 20:26:30 -0700

    merge intervals

--- LeetCode/Medium/Merge Intervals/C++/merge_intervals.cc
+++ LeetCode/Medium/Merge Intervals/C++/merge_intervals.cc
@@ -1,22 +1,25 @@
 class Solution {
 public:
-    static bool cmp(const vector<int>& a, const vector<int>& b) {
-        return a[0] < b[0];
-    }
-
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end(), cmp);
         vector<vector<int>> res;
-        vector<int> curr = intervals[0];
-        for (int i=1; i<intervals.size(); i++) {
-            if (intervals[i][0] <= curr[1]) 
-                curr[1] = max(curr[1], intervals[i][1]);
-            else {
-                res.push_back(curr);
-                curr = intervals[i];
+
+        sort(intervals.begin(), intervals.end());
+        int start = intervals[0][0];
+        int end =  intervals[0][1];
+        for (int i = 0; i < intervals.size(); i++) {
+            if (start == intervals[i][0] && end == intervals[i][1]) continue;
+
+            if (intervals[i][0] <= end) {
+                end = max(end, intervals[i][1]);
+            } else {
+                res.push_back({start, end});
+                start = intervals[i][0];
+                end = intervals[i][1];
             }
         }
-        res.push_back(curr);
+
+        res.push_back({start, end});
+
         return res;
     }
 };
