40bcb280e1e218242e8db50c2ecf52338279deb2
Puru Jaiswal <p96jcodes@gmail.com>
2024-11-04 01:06:06 -0800

    coin change

--- LeetCode/Medium/Coin Change/C++/coin_change.cc
+++ LeetCode/Medium/Coin Change/C++/coin_change.cc
@@ -1,45 +1,22 @@
-// ----- brute force -------
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        return recur(coins, amount);
-    }
-
-    int recur(vector<int> coins, int amt) {
-        if (amt < 0) return -1;
-        if (amt == 0) return 0;
-        int min_val = INT_MAX;
-        for (int i=0; i<coins.size(); i++) {
-            int take = recur(coins, amt-coins[i]);
-            if (take >= 0 && take < min_val) {
-                min_val = take + 1;
-            }
-        }
-        return min_val == INT_MAX ? -1 : min_val;
-    }
-};
-
-
-// ---- memo ------
-class Solution {
-public:
-    int coinChange(vector<int>& coins, int amount) {
-        vector<int> memo(amount+1);
+        vector<int> memo(amount + 1);
         return recur(coins, amount, memo);
     }
 
-    int recur(vector<int> coins, int amt, vector<int>& memo) {
-        if (amt < 0) return -1;
-        if (amt == 0) return 0;
-        if (memo[amt] != 0) return memo[amt];
-        int min_val = INT_MAX;
-        for (int i=0; i<coins.size(); i++) {
-            int take = recur(coins, amt-coins[i], memo);
-            if (take >= 0 && take < min_val) {
-                min_val = take + 1;
-            }
+    int recur(vector<int>& coins, int amount, vector<int>& memo) {
+        if (amount < 0) return -1;
+        if (amount == 0) return 0;
+        if (memo[amount] != 0) return memo[amount];
+        
+        int res = INT_MAX;
+
+        for (int i = 0; i < coins.size(); i++) {
+            int take = recur(coins, amount - coins[i], memo);
+            if (take >= 0 && take < res) res = take + 1;
         }
-        memo[amt] = min_val == INT_MAX ? -1 : min_val;
-        return memo[amt];
+
+        return memo[amount] = res == INT_MAX ? -1 : res;
     }
 };
