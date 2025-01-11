60be4f51887e43da32ed8e5cb7858bb97f0397bc
Puru Jaiswal <p96jcodes@gmail.com>
2025-01-11 00:16:21 -0800

    next alphabetical sequence

---
+++ Algorithms/Coding Interview Patterns/Two Pointers/next_alphbetical_seq.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string next_alpha_seq(string s) {
+    int pivot = s.length() - 2;
+    int right_succ = s.length() - 1;
+    while (pivot >= 0 && s[pivot] >= s[pivot + 1]) {
+        pivot--;
+    }
+    if (pivot == -1) {
+        reverse(s.begin(), s.end());
+        return s;
+    }
+
+    while (s[right_succ] <= s[pivot]) {
+        right_succ--;
+    }
+    
+    char c = s[pivot];
+    s[pivot] = s[right_succ];
+    s[right_succ] = c;
+
+    return s;
+}
+
+int main(int argc, char **argv) {
+    cout << next_alpha_seq("abcd") << "\n";
+    return 0;
+}
