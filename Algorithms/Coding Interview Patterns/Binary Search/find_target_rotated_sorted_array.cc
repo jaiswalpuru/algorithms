69b39f8c71fb60211f889579c30ad50bc9ae6671
Puru Jaiswal <p96jcodes@gmail.com>
2025-01-30 02:02:23 -0800

    find target in a rotated sorted array

---
+++ Algorithms/Coding Interview Patterns/Binary Search/find_target_rotated_sorted_array.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int find_target(vector<int> arr, int target) {
+    int l = 0;
+    int r = arr.size() - 1;
+
+    while (l < r) {
+        int mid = l + (r - l) / 2;
+        if (arr[mid] == target) {
+            return mid;
+        } else if (arr[l] <= arr[mid]) {
+            if (arr[l] <= target && target < arr[mid]) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        } else { 
+            if (arr[mid] < target && target <= arr[mid]) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+    }
+
+    return arr[l] == target ? l : -1;
+}
+
+int main(int argc, char** argv) {
+    cout <<find_target({8, 9, 1, 2, 3, 4, 5, 6, 7}, 1) << "\n";
+    return 0;
+}
